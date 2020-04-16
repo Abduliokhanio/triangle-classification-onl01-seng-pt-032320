e26dbd1b1ec8f1688bd803d977eaa4d9dc6149f5
Abdul Khan <akhan17@outlook.com>
2020-04-16 19:51:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,17 @@ class Triangle
     @trs = trs
 
     def kind
+
+      if (@uno,@dos,@trs == 0) 
+        raise RangeError
+      else 
       sides = [@uno, @dos, @trs].uniq
       type = case sides.length
         when 1 then :equilateral
         when 2 then :isosceles
         when 3 then :scalene
        end
-
+     end 
 
     end#kind method
 
