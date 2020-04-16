3a442e0d36a66f32ad95b00f5bf4cb364ae07961
Abdul Khan <akhan17@outlook.com>
2020-04-16 20:18:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
-end
+
+  attr_accessor :uno, :dos, :tres
+
+  def initialize(uno,dos,trs)
+
+    @uno = uno
+    @dos = dos
+    @trs = trs
+
+    def kind
+
+      if @uno > 0 && @dos > 0 && @trs > 0
+        sides = [@uno, @dos, @trs].uniq
+
+        unless @uno+@dos > @trs
+        raise TriangleError, "Does not satisfy triangle inequality"
+        end
+
+        unless @uno+@trs > @dos
+        raise TriangleError, "Does not satisfy triangle inequality"
+        end
+
+        unless @dos+@trs > @uno
+        raise TriangleError, "Does not satisfy triangle inequality"
+        end
+
+        type = case sides.length
+          when 1 then :equilateral
+          when 2 then :isosceles
+          when 3 then :scalene
+         end
+       else
+          raise TriangleError
+      end #if
+    end#kind method
+
+  end#initialization
+
+  class TriangleError < StandardError
+    def message
+      "nono"
+    end #message method
+  end #error
+end#class
